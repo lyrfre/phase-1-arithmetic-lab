87299faecb0a30fb633b3e0ab6e104f8a55f4ab5
Lyra Freund <lyrafreund@gmail.com>
2023-09-16 10:51:33 -0600

    co plete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 2 
+const num2 = 31
+const multiply = num1 * num2 
+const random = Math.floor(Math.random() + 1)
+const num3 = 24
+const num4 = 5
+const mod = num3 % num4
+const max = Math.max(1, 2, 3, 4, 5, 20)
+
+// function random(num){
+
+// }
